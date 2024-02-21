7289564f82f69e128d17f5d62317fe2b0a45216b
Aaron <aperkey2@huskers.unl.edu>
2024-02-21 15:43:54 -0600

    done

--- Assets/Scripts/Shooting Area/Timer.cs
+++ Assets/Scripts/Shooting Area/Timer.cs
@@ -8,25 +8,34 @@ public class Timer : MonoBehaviour
 {
 
     public float timeRemaining = 3;
-    public bool timeIsRunning = true;
+    public bool timeIsRunning;
     public TMP_Text timeText;
 
     // Start is called before the first frame update
     void Start()
     {
         // Starts the timer
-        timeIsRunning = true;
+        timeIsRunning = true; //TODO: Delete this line
     }
 
     // Update is called once per frame
     void Update()
     {
+        // TODO:  if button pushed then start timer and target game
+        //if ()
+        //{
+            //timeIsRunning = true;
+        //}
         if (timeIsRunning)
         {
             if (timeRemaining >= 0) 
             {
                 timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeIsRunning = false;
             }
 
         }
